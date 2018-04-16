792638500b1fcb6c42ac7f153564e65da74184c3
Dmitry Filonenko <feelll@bigmir.net>
2018-04-16 17:44:17 +0300

    Added button to save text file.

--- DirWinFace/DirWinFace/Form1.Designer.cs
+++ DirWinFace/DirWinFace/Form1.Designer.cs
@@ -31,13 +31,15 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(685, 406);
+            this.button1.Location = new System.Drawing.Point(12, 406);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.Size = new System.Drawing.Size(497, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Выбрать папку";
             this.button1.UseVisualStyleBackColor = true;
@@ -51,11 +53,33 @@
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(668, 406);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Сохранить в файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(529, 406);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -69,6 +93,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- DirWinFace/DirWinFace/Form1.cs
+++ DirWinFace/DirWinFace/Form1.cs
@@ -12,7 +12,9 @@ using System.Windows.Forms;
 namespace DirWinFace
 {
     public partial class Form1 : Form
-    {        
+    {
+        string _text = "";
+
         public Form1()
         {
             InitializeComponent();
@@ -33,19 +35,44 @@ namespace DirWinFace
 
         void Parse(string path)
         {
-            string[] allfiles = Directory.GetDirectories(path);
-            string res = "";
-            foreach (var item in allfiles)
+            try
             {
-                FileAttributes attr = File.GetAttributes(item);
-
-                if (attr.HasFlag(FileAttributes.Directory))
+                //string[] allfiles = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+                string[] allfiles = Directory.GetDirectories(path);
+                foreach (var item in allfiles)
                 {
-                    res += (item + Environment.NewLine);
-                }
+                    FileAttributes attr = File.GetAttributes(item);
 
+                    if (attr.HasFlag(FileAttributes.Directory))
+                    {
+                        _text += (item + Environment.NewLine);
+                    }
+                }
+                richTextBox1.Text = _text;
             }
-            richTextBox1.Text = res;
+            catch (Exception)
+            {
+                MessageBox.Show("Похоже нет доступа к некоторым папкам.");
+            }           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.FileName = "Список.txt";
+            savefile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(savefile.FileName))
+                    sw.WriteLine(_text);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _text = "";
+            richTextBox1.Text = "";
         }
     }
 }
