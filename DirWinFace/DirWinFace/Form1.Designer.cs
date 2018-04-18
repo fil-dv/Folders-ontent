9e98da1192110045d49baa95e89d3f09233d5ee8
Dmitry Filonenko <feelll@bigmir.net>
2018-04-18 10:50:55 +0300

    Parser work in other thread and generate Action about progress in work.

--- DirWinFace/DirWinFace/Form1.Designer.cs
+++ DirWinFace/DirWinFace/Form1.Designer.cs
@@ -33,13 +33,14 @@
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 406);
+            this.button1.Location = new System.Drawing.Point(354, 406);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(497, 23);
+            this.button1.Size = new System.Drawing.Size(166, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Выбрать папку";
             this.button1.UseVisualStyleBackColor = true;
@@ -55,9 +56,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(668, 406);
+            this.button2.Location = new System.Drawing.Point(678, 406);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.Size = new System.Drawing.Size(110, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "Сохранить в файл";
             this.button2.UseVisualStyleBackColor = true;
@@ -65,19 +66,29 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(529, 406);
+            this.button3.Location = new System.Drawing.Point(549, 406);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.Size = new System.Drawing.Size(114, 23);
             this.button3.TabIndex = 3;
-            this.button3.Text = "Очистить";
+            this.button3.Text = "Очистить ";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 408);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(311, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
@@ -95,6 +106,7 @@
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- DirWinFace/DirWinFace/Form1.cs
+++ DirWinFace/DirWinFace/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace DirWinFace
@@ -14,10 +10,35 @@ namespace DirWinFace
     public partial class Form1 : Form
     {
         string _text = "";
+        string _path = "";
+        int _selector = 0;
+        List<string> _allfiles = new List<string>();
+        public event Action<int> WorkInProcess;
 
         public Form1()
         {
             InitializeComponent();
+            InitialHandler();
+            InitCombo();
+        }
+
+        void InitialHandler()
+        {
+            WorkInProcess += Signal;
+        }
+
+        void Signal(int count)
+        {
+            this.Text = "Обработано записей: " + count;
+        }
+
+        private void InitCombo()
+        {
+            comboBox1.Items.Add("Вложенные каталоги");
+            comboBox1.Items.Add("Вложенные файлы");
+            comboBox1.Items.Add("Вложенные каталоги со всеми подкаталогами");
+            comboBox1.Items.Add("Вложенные файлы во всех подкаталогах");
+            comboBox1.SelectedIndex = 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -28,7 +49,38 @@ namespace DirWinFace
 
                 if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
                 {
-                    Parse(fbd.SelectedPath);
+                    _path = fbd.SelectedPath;
+                    Thread th = new Thread(new ThreadStart(StartParse));
+                    th.Start();
+                }
+            }
+        }
+
+        void StartParse()
+        {
+            Parse(_path);
+        }
+         
+        void ProcessFile(string path)
+        {
+            _allfiles.Add(path);
+        }
+
+        static void ApplyAllFiles(string folder, Action<string> fileAction)
+        {
+            foreach (string file in Directory.GetFiles(folder))
+            {
+                fileAction(file);
+            }
+            foreach (string subDir in Directory.GetDirectories(folder))
+            {
+                try
+                {
+                    ApplyAllFiles(subDir, fileAction);
+                }
+                catch
+                {
+                    // игнорируем файлы, к которым нет доступа...
                 }
             }
         }
@@ -37,22 +89,66 @@ namespace DirWinFace
         {
             try
             {
-                //string[] allfiles = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
-                string[] allfiles = Directory.GetDirectories(path);
-                foreach (var item in allfiles)
+                _text = "";
+                
+                switch (_selector)
                 {
-                    FileAttributes attr = File.GetAttributes(item);
+                    case 0: // Вложенные каталоги
+                        _allfiles = Directory.GetDirectories(path).ToList();
+                        break;
+                    case 1: // Вложенные каталоги и файлы
+                        _allfiles = Directory.GetFiles(path).ToList();
+                        break;
+                    case 2: // Вложенные каталоги со всеми подкаталогами
+                        _allfiles = Directory.GetDirectories(path, "*", SearchOption.AllDirectories).ToList();
+                        break;
+                    case 3: // Вложенные файлы во всех подкаталогах
+                        ApplyAllFiles(path, ProcessFile);
+                        break;
+                    default:
+                        _allfiles = Directory.GetDirectories(path).ToList();
+                        break;
+                }
 
-                    if (attr.HasFlag(FileAttributes.Directory))
+                int count = 0;
+
+                foreach (var item in _allfiles)
+                {                    
+                    if((++count % 100) == 1)
+                    {
+                        if (WorkInProcess != null)
+                        {
+                            WorkInProcess(count);
+                        }
+                    }
+
+                    try
                     {
                         _text += (item + Environment.NewLine);
                     }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        continue;
+                    }
                 }
-                richTextBox1.Text = _text;
+                
+                if (InvokeRequired)
+                {
+                    Action action = () =>
+                    {
+                        UpdateText();
+                    };
+                    Invoke(action);
+                }
+                else
+                {
+                    UpdateText();
+                } 
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                MessageBox.Show("Похоже нет доступа к некоторым папкам.");
+                MessageBox.Show("Похоже, что-то не сработало..." + Environment.NewLine + ex.Message );
             }           
         }
 
@@ -65,7 +161,10 @@ namespace DirWinFace
             if (savefile.ShowDialog() == DialogResult.OK)
             {
                 using (StreamWriter sw = new StreamWriter(savefile.FileName))
-                    sw.WriteLine(_text);
+                {
+                    sw.WriteLine(richTextBox1.Text);
+                }
+                
             }
         }
 
@@ -73,6 +172,17 @@ namespace DirWinFace
         {
             _text = "";
             richTextBox1.Text = "";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _selector = comboBox1.SelectedIndex;                       
+        }
+
+        private void UpdateText()
+        {
+            richTextBox1.Text += _text;
+            this.Text = "Содержимое папки";
         }
     }
 }
